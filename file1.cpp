dc082fbb36093543b30f1ace586f290e8306906c
Jilada Eccleston <jilada.eccleston@tier4.jp>
2021-03-03 18:45:19 +0900

    Add file1

---
+++ file1.cpp
@@ -0,0 +1,10 @@
+// Pull mirror
+
+#include <iostream>
+
+int main() {
+    std::cout << "This code was merged using pull mirroring";
+    std::cout << "This code was merged using push mirroring";
+    return 0;
+}
+
